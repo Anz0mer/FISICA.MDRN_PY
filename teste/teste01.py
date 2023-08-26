a479bead6c7ca655ef2e9ad9b805a9a0fc1162c0
Anna Carolina Zomer <117862465+Anz0mer@users.noreply.github.com>
2023-08-26 02:11:43 +0000

    teste

--- teste/teste01.py
+++ teste/teste01.py
@@ -3,28 +3,48 @@ def calcular_grandezas(entrada, valor):
     if entrada == "Em":
         Bm = valor
         I = (Bm ** 2) / 2
-        return f"Amplitude do Campo Magnético (Bm): {Bm}\nIntensidade (I): {I}"
+        return f"Amplitude do Campo Magnético (Bm): {Bm}\nIntensidade (I): {I}\n"
     elif entrada == "I":
         Bm = (2 * valor) ** 0.5
         Em = Bm
-        return f"Amplitude do Campo Elétrico (Em): {Em}\nAmplitude do Campo Magnético (Bm): {Bm}"
+        return f"Amplitude do Campo Elétrico (Em): {Em}\nAmplitude do Campo Magnético (Bm): {Bm}\n"
     elif entrada == "Bm":
         Em = valor
         I = (Em ** 2) / 2
-        return f"Amplitude do Campo Elétrico (Em): {Em}\nIntensidade (I): {I}"
+        return f"Amplitude do Campo Elétrico (Em): {Em}\nIntensidade (I): {I}\n"
     else:
-        return "Entrada inválida"
+        return "Entrada inválida\n"
 
-# Solicitar a entrada do usuário
-entrada_usuario = input("Digite a grandeza que você possui (Em, Bm ou I): ").strip()
+# Função para exibir o menu
+def exibir_menu():
+    print("Escolha a grandeza que você possui:")
+    print("1. Amplitude do Campo Elétrico (Em)")
+    print("2. Amplitude do Campo Magnético (Bm)")
+    print("3. Intensidade (I)")
+    print("0. Sair")
 
-# Verificar se a entrada é válida e, se for, solicitar o valor correspondente
-if entrada_usuario in ["Em", "Bm", "I"]:
-    valor_usuario = float(input(f"Digite o valor da grandeza {entrada_usuario}: "))
-    
-    # Calcular e imprimir as grandezas correspondentes
-    resultado = calcular_grandezas(entrada_usuario, valor_usuario)
-    print("\nResultado:")
-    print(resultado)
-else:
-    print("Entrada inválida. Por favor, insira Em, Bm ou I como entrada.")
+# Loop principal
+while True:
+    exibir_menu()
+    escolha = input("Digite o número da opção desejada: ")
+
+    if escolha == "1":
+        Em = float(input("Digite a Amplitude do Campo Elétrico (Em): "))
+        resultado = calcular_grandezas("Em", Em)
+        print("\nResultado:")
+        print(resultado)
+    elif escolha == "2":
+        Bm = float(input("Digite a Amplitude do Campo Magnético (Bm): "))
+        resultado = calcular_grandezas("Bm", Bm)
+        print("\nResultado:")
+        print(resultado)
+    elif escolha == "3":
+        I = float(input("Digite a Intensidade (I): "))
+        resultado = calcular_grandezas("I", I)
+        print("\nResultado:")
+        print(resultado)
+    elif escolha == "0":
+        print("Saindo do programa.")
+        break
+    else:
+        print("Opção inválida. Por favor, escolha uma opção válida.")
