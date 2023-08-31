0b6a948c8abcf2bff617a3c91b6792d55370b95d
Anna Carolina Zomer <117862465+Anz0mer@users.noreply.github.com>
2023-08-31 15:26:11 -0300

    Update codigoindo.py

--- teste/codigoindo.py
+++ teste/codigoindo.py
@@ -37,12 +37,12 @@ def calcular_frequencia_angular_com_f(frequencia):
 # Função para exibir o menu
 def exibir_menu():
     print("Escolha uma opção:")
-    print("1. Calcular Frequência da Onda em [Hz] com o valor [k em rad/m]")
-    print("2. Calcular Comprimento de Onda em [m] com o valor [k em rad/m]")
-    print("3. Calcular Frequência Angular da Onda em [rad/s] com o valor [k em rad/m]")
-    print("4. Calcular Comprimento da Onda em [m] com o valor [f em Hz]")
-    print("5. Calcular Número de Onda em [rad/m] com o valor [f em Hz]")
-    print("6. Calcular Frequência Angular em [rad/s] com o valor [f em Hz]")
+    print("1. k em rad/m")
+    print("2. f em Hz")
+    print("3. ")
+    print("4. ")
+    print("5. ")
+    print("6. ")
     print("0. Sair")
 
 # Loop principal
@@ -54,31 +54,25 @@ while True:
         numero_onda = float(input("Digite o número de onda (k) em rad/m: "))
         frequencia = calcular_frequencia(numero_onda)
         print(f"RESPOSTA: A frequência da onda é {frequencia} Hz.\n")
-    
-    elif escolha == "2":
-        numero_onda = float(input("Digite o número de onda (k) em rad/m: "))
+        
         comprimento_onda = calcular_comprimento_onda(numero_onda)
         print(f"RESPOSTA: O comprimento de onda da onda é {comprimento_onda} m.\n")
-   
-    elif escolha == "3":
-        numero_onda = float(input("Digite o número de onda (k) em rad/m: "))
+    
         frequencia_angular = calcular_frequencia_angular(numero_onda)
         print(f"RESPOSTA: A frequência angular da onda é {frequencia_angular} rad/s.\n") 
     
-    elif escolha == "4":
+    elif escolha == "2":
         frequencia = float(input("Digite a frequência da onda em Hz: "))
         comprimento_onda_f = calcular_comprimento_onda_com_f(frequencia)
         print(f"RESPOSTA: O comprimento de onda da onda é {comprimento_onda_f} m.\n")
 
-    elif escolha == "5":
-        frequencia_f = float(input("Digite a frequência da onda em Hz: "))
-        numero_onda_f = calcular_numero_onda(calcular_comprimento_onda_com_f(frequencia_f))
+        numero_onda_f = calcular_numero_onda(calcular_comprimento_onda_com_f(frequencia))
         print(f"RESPOSTA: O número de onda da onda é {numero_onda_f} rad/m.\n")
 
-    elif escolha == "6":
-        frequencia_f = float(input("Digite a frequência da onda em Hz: "))
-        frequencia_angular_f = calcular_frequencia_angular_com_f(frequencia_f)
+        frequencia_angular_f = calcular_frequencia_angular_com_f(frequencia)
         print(f"RESPOSTA: A frequência angular da onda é {frequencia_angular_f} rad/s.\n")
+
+    
     
     elif escolha == "0":
         print("Saindo do programa.")
